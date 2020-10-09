e48c10c89f0de48e3b43a06fe96f19eb9250e506
zhaomingyue <894580402@qq.com>
2020-10-09 09:05:58 +0800

    完成了index

--- assets/js/baseAPI1.js
+++ assets/js/baseAPI1.js
@@ -2,5 +2,17 @@ var baseURL = "http://ajax.frontend.itheima.net"
 
 // ajaxPrefilter拦截所有ajax请求:get/post/ajax
 $.ajaxPrefilter(function(params) {
-    params.url = baseURL + params.url
+    params.url = baseURL + params.url;
+    if (params.url.indexOf('/my/') !== -1) {
+        params.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+    params.complete = function(res) {
+        //判断:如果身份认证失败,跳转回登录页面
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            localStorage.removeItem('token');
+            location.href = '/login1.html'
+        }
+    }
 })
---
+++ assets/js/index1.js
@@ -0,0 +1,53 @@
+$(function() {
+    getUserInfo()
+})
+
+// 获取用户的基本信息
+function getUserInfo() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        headers: {
+            Authorization: localStorage.getItem('token')
+        },
+        success: function(res) {
+            console.log(res);
+            if (res.status !== 0) {
+                return layui.layer.msg('获取用户信息失败')
+            }
+            renderAvatar(res.data)
+        }
+    })
+}
+
+function renderAvatar(user) {
+    // 1.获取用户的名称
+    var name = user.nickname || user.username
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
+        // 按需渲染用户的头像
+    if (user.user_pic !== null) {
+        $('.layui-nav-img').attr('src', user.user_pic).show();
+        $('.text-avatar').hide()
+    } else {
+        $('.layui-nav-img').hide()
+        var first = name[0].toUpperCase()
+        $('.text-avatar').html(first).show()
+    }
+}
+
+var layer = layui.layer
+
+// 点击按钮，实现退出功能
+$('#btnLogout').on('click', function() {
+    // 提示用户是否确认退出
+    layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function(index) {
+        //do something
+        // 1. 清空本地存储中的 token
+        localStorage.removeItem('token')
+            // 2. 重新跳转到登录页面
+        location.href = '/login.html'
+
+        // 关闭 confirm 询问框
+        layer.close(index)
+    })
+})
